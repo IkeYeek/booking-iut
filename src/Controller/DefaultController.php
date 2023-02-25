3f02c3ccfc12cbab0340094fedef97f4db60fec0
Lucas Marquès <lucas.marques@u-bordeaux.fr>
2023-02-25 14:13:09 +0100

    *[Utilisation de la configuration]

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('default/index.html.twig');
+        $configuration = $em->getRepository(Configuration::class)->findOneBy([]);
+
+        return $this->render('default/index.html.twig', [
+            'placeName' => $configuration->getPlaceName(),
+            'placeAddress' => $configuration->getPlaceAddress(),
+            'iframeUrl' => 'https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d2794.035620333259!2d' . $configuration->getLatitude() . '!3d' . $configuration->getLongitude() . '!2m3!1f0!2f0!3f0!3m2!1i1044!2i768!4f13.1!3m2!1m1!2zNFCsXZiJzU4LjYiTiAwwrAzOSc1OS4yIkU!5e0'
+        ]);
     }
 }
